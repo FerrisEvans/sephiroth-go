265f56cdac69f57ad89e5e96eadb31c4436767b0
Ferris <ferris.evans@outlook.com>
2024-06-24 19:21:05 +0800

    zap init

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"go.uber.org/zap"
 	"sephiroth-go/core"
 	"sephiroth-go/init"
 )
+
+//go:generate go env -w GO111MODULE=on
+//go:generate go env -w GOPROXY=https://goproxy.cn,direct
+//go:generate go mod tidy
+//go:generate go mod download
 
 func main() {
 	core.Vp = core.Viper()
 	core.OtherInit()
 	core.Log = core.Zap()
+	zap.ReplaceGlobals(core.Log)
 	core.Db = init.Database()
 }
